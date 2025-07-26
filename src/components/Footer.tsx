39f21f394e67a7e01de95bef74590b6f1a250a8f
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-26 16:28:13 +0530

    Adding the footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-slate-800 flex justify-center items-center h-20 '>Made With ❤️ by Vividly</div>
+  )
+}
+
+export default Footer
