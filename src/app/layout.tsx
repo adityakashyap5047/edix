3a0d5b2d28842349dc7a21d032d854271b904fdd
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-26 10:55:38 +0530

    adding the components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Inter } from "next/font/google";
+
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "Vividly",
@@ -19,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body className={`${inter.className}`}>
         <ThemeProvider attribute={"class"} defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
