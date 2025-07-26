997be46dc6b6c0ec5ec50d4757563bec26366412
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-26 10:48:31 +0530

    adding the dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import React from 'react'
 
@@ -5,6 +6,8 @@ const page = () => {
   return (
     <div>
       <Image src={"/vividly/vividly-text.png"} alt="Vividly Logo" width={200} height={200} className='h-4 w-30' />
+      <Button variant="primary"> Click Me </Button>
+      <Button variant="glass"> Click Me </Button>
     </div>
   )
 }
