7dec639d457d79b75c1ed5eb3ac915fef4aa810f
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-08-04 11:21:44 +0530

    starting with the downloading the media

--- src/app/(main)/editor/_components/EditorTopBar.tsx
+++ src/app/(main)/editor/_components/EditorTopBar.tsx
@@ -279,7 +279,6 @@ const EditorTopBar = ({project}: {project: Project}) => {
                         )} 
                     </Button>
 
-                    {/* Export Dropdown */}
                     <DropdownMenu>
                         <DropdownMenuTrigger asChild>
                             <Button
