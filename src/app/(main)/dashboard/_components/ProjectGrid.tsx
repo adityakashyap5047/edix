3837bfd21edaf02055bf04c9aa54c23884f70634
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-08-19 00:27:45 +0530

    enhancing the functionality

--- src/app/(main)/dashboard/_components/ProjectGrid.tsx
+++ src/app/(main)/dashboard/_components/ProjectGrid.tsx
@@ -11,7 +11,7 @@ const ProjectGrid = ({projects, setProjects, setProjectCount}: {projects: Projec
     }
 
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6'>
+        <div className='mb-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6'>
             {projects.map((project) => (
                 <ProjectCard
                     key={project.id}
